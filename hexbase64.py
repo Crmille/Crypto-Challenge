1ccf1a7bbac6e088282127b9c8ac98a332ee9469
Chad Miller <crmille@gmail.com>
2017-08-07 10:28:54 -0500

    Update
    Solution to Problem 1

---
+++ hexbase64.py
@@ -0,0 +1,19 @@
+
+''' Converts hex to base64'''
+
+import base64
+from codecs import decode
+
+def main():
+   # b16 = bytearray('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'.decode('hex'))
+    b16 = decode('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d','hex')
+    b64 = base64.b64encode(b16)
+    
+    print(b16)
+    print(b64)
+    
+    assert(b64 == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
+    
+        
+if __name__== "__main__":
+    main()
